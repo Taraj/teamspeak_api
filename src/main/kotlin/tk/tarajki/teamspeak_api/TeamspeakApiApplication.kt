454f786bbb315138e18a0d0fffc0ba7d7fd63d87
Krzysiek <kbachon97@gmail.com>
2019-06-29 09:07:15 +0200

    Initial commit

---
+++ src/main/kotlin/tk/tarajki/teamspeak_api/TeamspeakApiApplication.kt
@@ -0,0 +1,11 @@
+package tk.tarajki.teamspeak_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TeamspeakApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<TeamspeakApiApplication>(*args)
+}
